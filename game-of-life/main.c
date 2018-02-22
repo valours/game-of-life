cc3b4eac5db921a968865165cef4f433b322be2e
valours <leflem.valentin@gmail.com>
2018-02-22 23:53:07 +0100

    simple interaction

--- game-of-life/main.c
+++ game-of-life/main.c
@@ -8,8 +8,13 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(int argc, char *argv[])
+{
+    int age = 0; // On initialise la variable à 0
+    
+    printf("Quel age avez-vous ? ");
+    scanf("%d", &age); // On demande d'entrer l'âge avec scanf
+    printf("Ah ! Vous avez donc %d ans !\n\n", age);
+    
     return 0;
 }
