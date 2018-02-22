3145daec4b9a61198cd7efae6d89f619a259ac97
valours <leflem.valentin@gmail.com>
2018-02-22 23:45:05 +0100

    Initial Commit

---
+++ game-of-life/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  game-of-life
+//
+//  Created by Valentin Le Flem on 22/02/2018.
+//  Copyright © 2018 Valentin Le Flem. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
